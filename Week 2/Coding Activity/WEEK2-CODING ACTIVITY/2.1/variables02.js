7bf98d3ff954f5b57febd8f75b2d02245b378e10
N S ANU <91176097+AnuMalar@users.noreply.github.com>
2021-10-18 19:10:05 +0530

    Delete Week 2/Coding Activity/WEEK2-CODING ACTIVITY directory

--- Week 2/Coding Activity/WEEK2-CODING ACTIVITY/2.1/variables02.js
+++
@@ -1,7 +0,0 @@
-let currentYear = "2021"
-console.log('current year result: ', currentYear); 
-//should be the value of the currentYear variable
-//don't change this line
-if (typeof module !== 'undefined') {
-  module.exports = currentYear;
-}
